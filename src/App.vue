3534fa8f068e05cf85671434e58908a13b23da17
Wilder Lizama <wilderlizama@gmail.com>
2018-10-16 22:01:38 -0500

    Primera prueba Vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="main-section">
+        Hello <b>Vue</b>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "mainApp"
+    }
+</script>
+
+<style>
+    .main-section {
+        margin: 0 auto;
+    }
+</style>
